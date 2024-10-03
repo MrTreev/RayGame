b29854b4366b1b11ee832e6b8810e2ca4b471476
MrTreev <mr.treev@gmail.com>
2024-10-03 18:32:52 +1000

    ignore clang global warning

--- src/core/math.cpp
+++ src/core/math.cpp
@@ -1,6 +1,10 @@
 #include "core/math.h" // IWYU pragma: keep
 
-uint64_t           core::math::detail::seed = 42;
+uint64_t core::math::detail::seed = 42;
+
+// Disable global constructor warnings for global random_device, as it is a singleton anyway
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wglobal-constructors"
+#pragma clang diagnostic ignored "-Wexit-time-destructors"
 std::random_device core::math::detail::dev;
-#if defined(__cpp_lib_philox_engine)
-#endif
+#pragma clang diagnostic pop
