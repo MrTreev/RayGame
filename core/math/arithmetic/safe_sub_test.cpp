61d978c98dd59f3b03272c0e88c581d391b6e6e8
MrTreev <mr.treev@gmail.com>
2024-11-30 12:23:30 +1100

    remove cmake

--- core/math/arithmetic/safe_sub_test.cpp
+++ core/math/arithmetic/safe_sub_test.cpp
@@ -6,22 +6,15 @@ TEST_SUITE("core::math::safe_sub") {
     using core::math::MathRule::ALLOW;
     using core::math::MathRule::CLAMP;
     using core::math::MathRule::STRICT;
+    using test::lowest;
     TEST_CASE_TEMPLATE("in-range", T, FWINT_PAIRS) {
-        using a_t   = T::A;
-        using b_t   = T::B;
-        const a_t a = 2;
-        const b_t b = 1;
-        CHECK_EQ(
-            safe_sub<a_t, STRICT>(test::lowest<a_t>() + a, a),
-            test::lowest<a_t>()
-        );
-        CHECK_EQ(
-            safe_sub<a_t, STRICT>(
-                test::lowest<a_t>() + core::math::numeric_cast<a_t>(b),
-                b
-            ),
-            test::lowest<a_t>()
-        );
+        using a_t    = T::A;
+        using b_t    = T::B;
+        const a_t a  = 2;
+        const b_t b  = 1;
+        const a_t ba = 1;
+        CHECK_EQ(safe_sub<a_t, STRICT>(lowest<a_t>() + a, a), lowest<a_t>());
+        CHECK_EQ(safe_sub<a_t, STRICT>(lowest<a_t>() + ba, b), lowest<a_t>());
         CHECK_EQ(safe_sub<a_t, STRICT>(a, b), 1);
         CHECK_EQ(safe_sub<a_t, CLAMP>(a, b), 1);
         CHECK_EQ(safe_sub<a_t, ALLOW>(a, b), 1);
@@ -30,11 +23,8 @@ TEST_SUITE("core::math::safe_sub") {
         using a_t     = T::A;
         using b_t     = T::B;
         const b_t sub = 1;
-        CHECK_THROWS(safe_sub<a_t, STRICT>(test::lowest<a_t>(), sub));
-        CHECK_NOTHROW(safe_sub<a_t, ALLOW>(test::lowest<a_t>(), sub));
-        CHECK_EQ(
-            safe_sub<a_t, CLAMP>(test::lowest<a_t>(), sub),
-            test::lowest<a_t>()
-        );
+        CHECK_THROWS(safe_sub<a_t, STRICT>(lowest<a_t>(), sub));
+        CHECK_NOTHROW(safe_sub<a_t, ALLOW>(lowest<a_t>(), sub));
+        CHECK_EQ(safe_sub<a_t, CLAMP>(lowest<a_t>(), sub), lowest<a_t>());
     }
 }
