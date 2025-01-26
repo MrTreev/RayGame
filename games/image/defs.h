2b85f2bfe991e0eaf868e882acde8ba2de51084c
MrTreev <mr.treev@gmail.com>
2025-01-26 21:12:15 +1100

    yeee

--- games/image/defs.h
+++ games/image/defs.h
@@ -1,7 +1,14 @@
 #include "raygame/core/types.h"
 #include <array>
+#include <ranges>
 
 namespace resources {
-[[maybe_unused]]
-extern const std::array<uint8_t, (92UL * 96UL * 4UL) + 1> icon_argb8888;
+
+namespace detail {
+template<typename T, size_t N>
+using img_t =
+    std::ranges::take_view<std::views::all_t<const std::array<T, N>&>>;
+} // namespace detail
+
+extern const detail::img_t<uint8_t, 35'329> img_argb8888;
 } // namespace resources
--- games/image/icon.cpp
+++ games/image/icon.cpp
@@ -4,4 +4,7 @@
 namespace resources {
 const std::array<uint8_t, (92UL * 96UL * 4UL) + 1> icon_argb8888 =
     std::to_array(cottage_image.pixel_data);
+
+const detail::img_t<uint8_t, 35'329> img_argb8888 =
+    std::ranges::views::take(icon_argb8888, icon_argb8888.size() - 1);
 } // namespace resources
