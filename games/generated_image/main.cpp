8d36c3b6b3bdad70cef7432675514753e6e25c1b
MrTreev <mr.treev@gmail.com>
2026-03-13 14:40:26 +1100

    Fix Generation

--- games/generated_image/main.cpp
+++ games/generated_image/main.cpp
@@ -1,6 +1,9 @@
+#include "generated/generated_png_image.h"
 #include "raygame/core/application/application.h"
 
 int main() {
     core::Application mywindow{};
-    while (mywindow.next_frame()) {}
+    while (mywindow.next_frame()) {
+        mywindow.draw(generated_png_image::pngtest);
+    }
 }
